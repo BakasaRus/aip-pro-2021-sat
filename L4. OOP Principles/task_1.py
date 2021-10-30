703d097ce9c84f85f3a4d6d1a12132bac3cff548
Nikolay Andreev <bakasaru@list.ru>
2021-10-30 20:07:48 +0300

    Add solution for task 1 in lesson 4

---
+++ L4. OOP Principles/task_1.py
@@ -0,0 +1,45 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dist(self, other):
+        if isinstance(other, Point):
+            return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
+
+
+class Vector(Point):
+    def length(self):
+        return super().dist(Point(0, 0))
+
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        if isinstance(other, (float, int)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+
+    def __rmul__(self, other):
+        if isinstance(other, (float, int)):
+            return Vector(self.x * other, self.y * other)
+
+    def __str__(self):
+        return f'Vector<x={self.x}, y={self.y}>'
+
+
+vector = Vector(5, 0)
+another = Vector(0, 5)
+third = Vector(1, 3)
+print(vector.length())
+print(vector + another)
+print(vector - another)
+print(third * 5)
+print(3 * third)
+print(vector * third)
